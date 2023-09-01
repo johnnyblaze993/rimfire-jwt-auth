1aabf0425e8a6cfb3b5fa33c60f48c3f7b86c82e
alvarezjo <jonathan.alvarez@axientcorp.com>
2023-09-01 12:15:58 -0500

    clean repo

--- src/main/java/com/rimfire/repositories/UsersRepository.java
+++ src/main/java/com/rimfire/repositories/UsersRepository.java
@@ -5,7 +5,6 @@ import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 import java.util.List;
-//import the query
 import io.micronaut.data.annotation.Query;
 import java.util.UUID;
 
@@ -13,10 +12,12 @@ import java.util.UUID;
 public interface UsersRepository extends CrudRepository<Users, UUID> {
 
     @Query("SELECT * FROM rimfirede.users WHERE username = :username")
-    Users findByUsername(String username); // make sure 'username' matches the case of your entity field
+    Users findByUsername(String username);
     
 
     
     @Query("SELECT * FROM rimfirede.users")
     List<Users> getAllUsers();
+
+    
 }
